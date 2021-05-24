a46ddf5bd4bdff2f5f8cdfaaae1131e3bfc6302f
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2021-05-25 08:36:10 +1000

    add jaycar logo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,15 @@
+import jclogo from "../jclogo.svg";
 import NewFileModal from "./NewFileModal";
 export default function Nav() {
   return (
     <div className="is-flex is-flex-align-center m-4">
-      <h3 className="is-3 title mb-0">Jaycar Stock Locator</h3>
+      <img
+        src={jclogo}
+        alt="Jaycar Logo"
+        className="box p-0 m-0 mr-2"
+        style={{ maxHeight: "80px" }}
+      />
+      <h3 className="is-3 title mb-0">Stock Locator</h3>
       <span style={{ flex: 1, justifySelf: "stretch", alignSelf: "stretch" }} />
       {/* <div>Date</div> */}
       <div className="buttons">
