49548787227a166583df04f45c8b1b9738dbe7fd
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-12-02 05:47:18 +0000

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'JaycarSort',
+        short_name: 'JaycarSort',
+
+        theme_color: '#ffffff',
+        background_color: '#0c254c',
+        display: 'standalone',
+      },
+    }),
+  ],
 });
