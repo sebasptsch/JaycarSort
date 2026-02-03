2b0d6efb8fe5bdf356bfcd5879b09091f0d3063d
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2026-02-03 14:03:43 +1100

    base url

--- src/main.tsx
+++ src/main.tsx
@@ -39,6 +39,7 @@ const router = createRouter({
 	// Since we're using React Query, we don't want loader calls to ever be stale
 	// This will ensure that the loader is always called when the route is preloaded or visited
 	defaultPreloadStaleTime: 0,
+	basepath: import.meta.env.BASE_URL,
 	scrollRestoration: true,
 });
 
