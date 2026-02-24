9ac73ed78eb7e2cff5ba052c7f8d2d2d9810c95f
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2026-02-24 13:33:14 +1100

    fix darwin string

--- src/lib/isTauri.ts
+++ src/lib/isTauri.ts
@@ -1,16 +1,24 @@
-import type { OsType } from "@tauri-apps/plugin-os";
-
 // @ts-expect-error Tauri is defined true or undefined
 export const isTauri = !!window.isTauri;
 
+type RustTripplePlatforms =
+	| "windows"
+	| "android"
+	| "darwin"
+	| "linux"
+	| "ios"
+	| "androideabi";
+
 export const currentPlatform = !isTauri
 	? "web"
-	: (import.meta.env.TAURI_ENV_PLATFORM as OsType);
+	: (import.meta.env.TAURI_ENV_PLATFORM as RustTripplePlatforms);
 
-export const isDesktop = ["linux", "macos", "windows"].includes(
+export const isDesktop = ["linux", "darwin", "windows"].includes(
 	currentPlatform,
 );
 
-export const isMobile = ["android", "ios"].includes(currentPlatform);
+export const isMobile = ["android", "ios", "androideabi"].includes(
+	currentPlatform,
+);
 
 export const isWeb = currentPlatform === "web";
